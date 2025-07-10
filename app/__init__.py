7f3130cd9d2559b97753717d2ebaa2f55b2333a4
Pulakesh Mondal <pulak4467@gmail.com>
2025-07-10 19:36:22 +0530

    Updated

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,9 @@ def create_app():
     # MongoDB config
     app.config["MONGO_URI"] = "mongodb://localhost:27017/webhook_db"
     mongo.init_app(app)
+
+    with app.app_context():
+        mongo.db.events.drop()
     
     # registering all the blueprints
     app.register_blueprint(webhook)
